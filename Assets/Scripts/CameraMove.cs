bfd0e1cb91822ecabe6dc3b811ac99e69ce8b84c
VITU-PC\victo <victorhbl12@hotmail.com>
2018-03-27 23:41:50 -0300

    Projeto final de RV
    Estilingue humano usando VRKT, SteamVR e o HTC Vive

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+	public float speed;
+
+	void Update () {
+		if(Input.GetKey(KeyCode.DownArrow)) RotateCameraOnAxis (Vector3.left, Vector3.down);
+		if(Input.GetKey(KeyCode.UpArrow)) RotateCameraOnAxis (Vector3.right, Vector3.up);
+	}
+
+	void RotateCameraOnAxis(Vector3 rotateDir, Vector3 translateDir){
+		transform.Rotate (rotateDir * Time.deltaTime * speed);
+		transform.Translate (translateDir * Time.deltaTime * speed/10);
+	}	
+}
---
+++ Assets/Scripts/CameraThrow.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraThrow : MonoBehaviour {
+
+	public float force;
+	public float maxForce;
+	public Slider slider;
+	private Rigidbody rb;
+	private bool throwed;
+	private float finalForce;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		throwed = false;
+		finalForce = 0;
+	}
+
+	void FixedUpdate () {
+		if(Input.GetKey(KeyCode.Space) && !throwed && finalForce < maxForce){
+			finalForce += Time.deltaTime * force;
+		}
+
+		if (Input.GetKeyUp (KeyCode.Space) && !throwed) {
+			throwed = true;
+			ApplyForce (rb, finalForce);
+		}
+
+		if (throwed && finalForce > 0) {
+			finalForce -= Time.deltaTime * force * 2;
+		}
+		UpdateSliderValue (finalForce, 0 , maxForce);
+	}
+
+	float Map (float value, float fromSource, float toSource, float fromTarget, float toTarget){
+        return (value - fromSource) / (toSource - fromSource) * (toTarget - fromTarget) + fromTarget;
+    }
+
+	void ApplyForce(Rigidbody rb, float force){
+		rb.useGravity = true;
+		rb.AddForce(this.transform.forward * force);
+	}
+
+	void UpdateSliderValue (float value, float min, float max){
+		value = Map (value, min, max, 0, 1);
+		slider.value = value;
+	}
+}
